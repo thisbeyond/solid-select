881dbed82be160281c6a0bb7f1d21924da8fda08
Martin Pengelly-Phillips <dev@thisbeyond.com>
2023-04-09 15:54:51 +0100

    Make async options throttled by default.
    Reuse solid-primitives throttle implementation - license at
    https://github.com/solidjs-community/solid-primitives/blob/main/packages/scheduled/LICENSE

--- src/create-async-options.tsx
+++ src/create-async-options.tsx
@@ -1,66 +1,10 @@
-import { createSignal, createResource } from "solid-js";
+import { createSignal, createResource, getOwner, onCleanup } from "solid-js";
 
 import { Option } from "./create-select";
 
 const createAsyncOptions = (
-  fetcher: (inputValue: string) => Promise<Option[]>
-) => {
-  const [inputValue, setInputValue] = createSignal("");
-  const [asyncOptions] = createResource(inputValue, fetcher, {
-    initialValue: [],
-  });
-
-  return {
-    get options() {
-      return asyncOptions();
-    },
-    get loading() {
-      return asyncOptions.loading;
-    },
-    onInput: setInputValue,
-    readonly: false,
-  };
-};
-
-type variableArgumentLambda = (...args: any[]) => any
-
-/**
- * applies only the last call to lambda that occured within a interval of time [threshhold] in ms, or the first one if no one occured before, default threshold is 250ms, yields a function.
- */ 
- function throttle(fn: variableArgumentLambda, threshhold: number = 250): variableArgumentLambda {
-
-  let last: any, deferTimer: any;
-  return  function(this: any) {
-    const now = +new Date,
-        args = arguments as unknown as any[],
-        ctx = this;
-    if (last && now < last + threshhold) {
-      // hold on to it
-      clearTimeout(deferTimer);
-      deferTimer = setTimeout( () => {
-        last = now;
-        fn.apply(ctx, args);
-      }, threshhold);
-    } else {
-      last = now;
-      fn.apply(ctx, args);
-    }
-  };
-}
-
-/**
- * creates a derivation from AsyncOptions that does not forward all consecutive input event to the fetcher lambda, but only those that occur outside of a specific time interval.
- * This helps to mitigate performance problems due to heavy I/O
- * 
- * consider the following example of events [A..E]
- * A..100ms..B..150ms..C..300ms..D..50ms..E
- * with a default threshhold of 250ms only Event A..C..E are considered
- * @param fetcher 
- * @param timeout 
- * @returns 
- */
-const createThrottledAsyncOptions = (
-  fetcher: (inputValue: string) => Promise<Option[]>, timeout?: number
+  fetcher: (inputValue: string) => Promise<Option[]>,
+  timeout = 250
 ) => {
   const [inputValue, setInputValue] = createSignal("");
   const [asyncOptions] = createResource(inputValue, fetcher, {
@@ -79,4 +23,30 @@ const createThrottledAsyncOptions = (
   };
 };
 
-export { createAsyncOptions, createThrottledAsyncOptions };
+// From https://github.com/solidjs-community/solid-primitives/blob/main/packages/scheduled/LICENSE
+const throttle = (callback: (...args: any) => any, threshold = 250) => {
+  let isThrottled = false,
+    timeoutId: ReturnType<typeof setTimeout>,
+    lastArgs: Parameters<typeof callback>;
+
+  const throttled: typeof callback = (...args) => {
+    lastArgs = args;
+    if (isThrottled) return;
+    isThrottled = true;
+    timeoutId = setTimeout(() => {
+      callback(...lastArgs);
+      isThrottled = false;
+    }, threshold);
+  };
+
+  const clear = () => {
+    clearTimeout(timeoutId);
+    isThrottled = false;
+  };
+
+  if (getOwner()) onCleanup(clear);
+
+  return Object.assign(throttled, { clear });
+};
+
+export { createAsyncOptions };
