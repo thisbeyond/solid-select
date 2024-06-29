b566916443e10320045229ef3e503e34d1721838
Martin Pengelly-Phillips <dev@thisbeyond.com>
2024-06-29 17:41:33 +0100

    Fix build and watch issues.
    Avoid errors caused by trying to manage
    `style.css` through `tsup-preset-solid`. Instead,
    move it to public folder and use tsup option to
    copy it directly. This then avoids errors caused
    by trying to generate a declaration file for a CSS
    file and also makes the package configuration
    simpler.
    
    In addition, cleanup tsconfig to remove redundant
    options and use more appropriate module resolution
    type.

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,36 +2,17 @@ import { defineConfig } from "tsup";
 import * as preset from "tsup-preset-solid";
 
 const preset_options: preset.PresetOptions = {
-  entries: [
-    {
-      entry: "./src/index.tsx",
-      dev_entry: true,
-    },
-    {
-      name: "style.css",
-      entry: "./src/style.css",
-    },
-  ],
+  entries: {
+    entry: "./src/index.tsx",
+    dev_entry: true,
+  },
   drop_console: true,
 };
 
 export default defineConfig((config) => {
+  config.publicDir = "public";
   const watching = !!config.watch;
-
-  const parsed_data = preset.parsePresetOptions(preset_options, watching);
-
-  if (!watching) {
-    const package_fields = preset.generatePackageExports(parsed_data);
-
-    // Force format for style.css import.
-    package_fields.exports["./style.css"].import = "./dist/style.css/index.css";
-    package_fields.typesVersions = {};
-
-    console.log(
-      `\npackage.json: \n${JSON.stringify(package_fields, null, 2)}\n\n`,
-    );
-    preset.writePackageJson(package_fields);
-  }
-  const result = preset.generateTsupOptions(parsed_data);
-  return result;
+  return preset.generateTsupOptions(
+    preset.parsePresetOptions(preset_options, watching),
+  );
 });
